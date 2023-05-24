05a1c3cc57794465711a4f53ea4d0b2e52cb2ec0
Vz Tong <vzz95559@gmail.com>
2023-05-24 20:50:21 +0700

    Add project files.

---
+++ Controllers/AccountsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace test1.Controllers
+{
+    public class AccountsController : Controller
+    {
+        // GET: Accounts
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
